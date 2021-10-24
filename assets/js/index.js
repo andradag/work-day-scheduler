bd604343c999b613dc09731c5f318ac0f32b918a
Andrada Gacichevici <andradagacichevici@gmail.com>
2021-10-24 21:14:06 +0100

    rendering hour block

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,11 @@
+const workingHours = [
+    {
+      Hour: "9am",
+      Event: $("#user-input"),
+    },
+  ];
+  
+
 const onLoad = function () {
   // on record load, check local storage
   checkLocalStorage();
@@ -11,29 +19,59 @@ const onLoad = function () {
   //render Clock
   renderClock();
 
+  //set Present / Future color coordination
+  isPresent();
+
   //render Hour Block
   renderHourBlocks();
 
-  //set Present / Future color coordination
-  isPresent();
-};
+  const checkLocalStorage = function () {
+    const dataFromLS = localStorage.getItem("user-input");
 
-const checkLocalStorage = function () {
-  // function to check if anything is in local storage
-  // if so, return data
-  // if not, set default value
-};
+    if (!dataFromLS) {
+      console.log("no data");
+    } else {
+      console.log("data");
+    }
 
-const renderClock = function () {
-  // function to render current day and time
-};
+  const renderClock = function () {
+    function update() {
+        $("#clock").html(moment().format("D. MMMM YYYY H:mm:ss"));
+      }
+    
+      setInterval(update, 1000);
+  };
 
-const renderHourBlocks = function () {
-  // render hour blocks
-};
+  const renderHourBlocks = function () {
+    // render hour blocks
 
-const isPresent = function () {
-  // if present - set color to --aquamarine
-  // if false - set color to --blue-violet
-  // if future - set color to --azure
+    const userInput = $("<textarea/>").attr("id", "user-event");
+
+    const timeLabel = $("<label>", {
+      name: "user-event",
+      id: "user-event",
+      class: "time-of-day",
+    });
+
+    const saveBtn = $("<button/>", {
+      text: "Save Event",
+      id: "save-btn",
+      class: "saveBtn",
+    });
+
+    const eventContainer = $("<div>", {
+      class: "event-container",
+    });
+
+    eventContainer.append(timeLabel, userInput, saveBtn);
+    const container = $("#container");
+    container.append(eventContainer);
+  };
+
+  const isPresent = function () {
+    // if present - set color to --aquamarine
+    // if false - set color to --blue-violet
+    // if future - set color to --azure
+  };
 };
+onLoad();
